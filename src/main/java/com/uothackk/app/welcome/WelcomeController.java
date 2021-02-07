c9249b6be91f833b37546956d55998f0d5b9cd5a
Willy Aguirre <marti1125@gmail.com>
2021-02-07 09:36:13 -0300

    added welcome page

---
+++ src/main/java/com/uothackk/app/welcome/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.uothackk.app.welcome;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/welcome")
+public class WelcomeController {
+
+    @GetMapping
+    public String welcome() {
+        return "welcome";
+    }
+
+}
