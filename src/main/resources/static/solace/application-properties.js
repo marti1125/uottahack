4c73cc574cad58015f2b5e7e89dc6a64b7f5a149
Willy Aguirre <marti1125@gmail.com>
2021-02-07 22:47:26 -0300

    added integration with solace for live-chat

---
+++ src/main/resources/static/solace/application-properties.js
@@ -0,0 +1,9 @@
+connectOptions = {
+	sBROKERURL: 'wss://mr1n34cqfh65tz.messaging.solace.cloud:443',
+	sVPN: 'reach-chat',
+	sUSERNAME: 'solace-cloud-client',
+	sPASSWORD: '3mgm5duaht7ubgto0aqdm9mtlt',
+	sPublishTopic: 'reach/chat',
+	sSubscribeTopic: 'reach/chat',
+	sReceiveQueue: ''
+}
